385eba71bba44b0710f85462e2c4ca0ac77f4a41
Solomonraj S <53432340+SRaju07@users.noreply.github.com>
2025-11-16 20:36:40 +0530

    Add files via upload

---
+++ MultipleFunctions.py
@@ -0,0 +1,23 @@
+class MultipleFunctions():
+    def subfields():
+        print("Sub-fields in AI are: ")
+        message="Sub-fields in AI are: "
+        print("Machine Learning")
+        message="Machine Learning"
+        print("Neural Networks")
+        message="Neural Networks"
+        print("Vision")
+        message="Vision"
+        print("Robotics")
+        message="Robotics"
+        print("Speech Processing")
+        message="Speech Processing"
+        print("Natural Language Processing")
+        message="Natural Language Processing"
+    def OddEven():
+        num=int(input("Enter a number"))
+        if(num%2==0):
+            print(num, " is Even Number")
+        else:
+            print(num, " is Odd Number")
+            return num
